e585c411a42761d51d175facafc401256b6a9308
Jacari Harper <jacarihr@gmail.com>
2022-07-12 19:41:44 -0700

    Include FrameBuffer class into Modlulus namespace

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -1,24 +1,25 @@
 #pragma once
 
 #include "texture.h"
+namespace Modlulus{
+	class FrameBuffer{
+		public:
+			FrameBuffer();
+			~FrameBuffer();
 
-class FrameBuffer{
-	public:
-		FrameBuffer();
-		~FrameBuffer();
+			bool init(int bufferWidth, int bufferHeight, bool multisample=false);
+			
+			void bind(GLenum target);
+			void bindTexture();
+			void unbindTexture();
 
-		bool init(int bufferWidth, int bufferHeight, bool multisample=false);
-		
-		void bind(GLenum target);
-		void bindTexture();
-		void unbindTexture();
-
-	private:
-		unsigned int mFrameBufferID;
-		unsigned int mRenderBufferID;
-		//Only used when generating multisampled gexture
-		unsigned int mmsFBOTextureID;
-		
-		//Non-multisampled texture
-		Modulus::Texture* mFBOTexture; 
-};
+		private:
+			unsigned int mFrameBufferID;
+			unsigned int mRenderBufferID;
+			//Only used when generating multisampled gexture
+			unsigned int mmsFBOTextureID;
+			
+			//Non-multisampled texture
+			Modulus::Texture* mFBOTexture; 
+	};
+}
