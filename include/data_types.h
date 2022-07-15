0ff7bccddd8b6418ec3c69b5fbb5da0af3a29297
Jacari Harper <jacarihr@gmail.com>
2022-07-14 19:58:02 -0700

    Add constructors for Vertex and Material types

--- include/data_types.h
+++ include/data_types.h
@@ -5,12 +5,16 @@
 namespace Modulus{
 	
 	struct Vertex{
+		Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 t) :
+			Position(p), Normal(n), TexCoordinate(t) {}
 		glm::vec3 Position;
 		glm::vec3 Normal;
 		glm::vec2 TexCoordinate;
 	};
 
 	struct Material{
+		Material(Texture* tx, std::string ty, std::string pa) :
+			texture(tx), type(ty), path(pa) {}
 		Texture* texture;
 		std::string type;
 		std::string path;
