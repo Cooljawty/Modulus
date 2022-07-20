ad9bac078f93b1fd06c5cce70cf9d1ddb522e3fc
Jacari Harper <jacarihr@gmail.com>
2022-07-19 19:20:38 -0700

    Remove constructors for Vertex and Material

--- include/data_types.h
+++ include/data_types.h
@@ -5,16 +5,12 @@
 namespace Modulus{
 	
 	struct Vertex{
-		Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 t) :
-			Position(p), Normal(n), TexCoordinate(t) {}
 		glm::vec3 Position;
 		glm::vec3 Normal;
 		glm::vec2 TexCoordinate;
 	};
 
 	struct Material{
-		Material(Texture* tx, std::string ty, std::string pa) :
-			texture(tx), type(ty), path(pa) {}
 		Texture* texture;
 		std::string type;
 		std::string path;
